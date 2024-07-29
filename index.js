dc8fede8c53afa7febd04cb6702664f2ba6e0ec0
Lusalvis <salvislukas@gmail.com>
2024-07-29 14:38:10 +0300

    js-intro

---
+++ index.js
@@ -0,0 +1,44 @@
+//vienos eilutes komentaras
+
+/*
+keliu 
+eiluciu
+komentaras
+*/
+
+/*
+Kintamieji:(duomenu tipai)
+    -number
+    -string (tekstas)
+    -boolean (true/false)
+    -arrat (sarasas)
+    -object (kompleksiskas duomuo/struktura)
+    -null
+    -undefined
+salygos/palyginimo (if):
+    =boolean logika
+    -if
+    -switch
+    -ternary
+ciklai (for):
+    -for
+    -for-of
+    -for-in
+    -while
+    -do-while
+    -cikliski metodiai:
+        -map
+        -filter
+        -sort
+        -reduce
+        -foreach
+        -N+1...
+function:
+    -iprastos deklaruotinos
+    -anonimines (priskirtos kintamajam)
+    -rodyklines (arrow)
+class: (oop)
+    -class
+    -inheri
+events listeners
+*/
