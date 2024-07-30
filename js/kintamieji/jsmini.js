b5ba1efbab9376f301a4ec9036d2ba414eb9f785
Lusalvis <salvislukas@gmail.com>
2024-07-30 13:33:29 +0300

    nd

---
+++ js/kintamieji/jsmini.js
@@ -0,0 +1,45 @@
+//1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+let num1 = 10;
+console.log(num1);
+
+let num2 = 20;
+console.log(num2);
+
+let num3 = 30;
+console.log(num3);
+console.log('')
+console.log('')
+
+//2.Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+let text1 = "Labas";
+console.log(text1);
+
+let text2 = "JavaScript";
+console.log(text2);
+
+let text3 = "Pasauli";
+console.log(text3);
+console.log('')
+console.log('')
+
+// 3. Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+let numb1 = [1, 2, 3 ,4 ,5];
+console.log(numb1);
+
+let numb2 = [10, 20, 30, 40, 50];
+console.log(numb2);
+
+let numb3 = [100, 200, 300, 400, 500];
+console.log(numb3);
+console.log('')
+console.log('')
+
+// 4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+let textb1 = ["vienas", "du", "trys", "keturi", "penki"];
+console.log(textb1);
+
+let textb2 = ["a", "b", "c", "d", "e"];
+console.log(textb2);
+
+let textb3 = ["obuolys", "kebabas", "burgeris", "arbuzas", "pica"];
+console.log(textb3);
