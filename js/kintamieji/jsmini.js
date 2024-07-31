991b65b7dc1e617a5f0491389712246a73d40482
Lusalvis <salvislukas@gmail.com>
2024-07-31 08:41:50 +0300

    minijs

--- js/kintamieji/jsmini.js
+++ js/kintamieji/jsmini.js
@@ -1,35 +1,35 @@
 //1. Sukurti 3 kintamuosius su skaiciaus tipo reiksmemis
-let num1 = 10;
+const num1 = 10;
 console.log(num1);
 
-let num2 = 20;
+const num2 = 20;
 console.log(num2);
 
-let num3 = 30;
+const num3 = 30;
 console.log(num3);
 console.log('')
 console.log('')
 
 //2.Sukurti 3 kintamuosius su teksto tipo reiksmemis
-let text1 = "Labas";
+const text1 = "Labas";
 console.log(text1);
 
-let text2 = "JavaScript";
+const text2 = "JavaScript";
 console.log(text2);
 
-let text3 = "Pasauli";
+const text3 = "Pasauli";
 console.log(text3);
 console.log('')
 console.log('')
 
 // 3. Sukurti 3 saraso tipo kintamuosius su penkiomis skaiciu tipo reiksmemis
-let numb1 = [1, 2, 3 ,4 ,5];
+const numb1 = [1, 2, 3 ,4 ,5];
 console.log(numb1);
 
-let numb2 = [10, 20, 30, 40, 50];
+const numb2 = [10, 20, 30, 40, 50];
 console.log(numb2);
 
-let numb3 = [100, 200, 300, 400, 500];
+const numb3 = [100, 200, 300, 400, 500];
 console.log(numb3);
 console.log('')
 console.log('')
@@ -38,10 +38,10 @@ console.log('')
 let textb1 = ["vienas", "du", "trys", "keturi", "penki"];
 console.log(textb1);
 
-let textb2 = ["a", "b", "c", "d", "e"];
+const textb2 = ["a", "b", "c", "d", "e"];
 console.log(textb2);
 
-let textb3 = ["obuolys", "kebabas", "burgeris", "arbuzas", "pica"];
+const textb3 = ["obuolys", "kebabas", "burgeris", "arbuzas", "pica"];
 console.log(textb3);
 
 //Kintamųjų palyginimas
